2bd076ab02fc948865b65545b8b82e601d5af917
Anka <zanhaa56@gmail.com>
2025-03-20 17:24:54 +0800

    inheritance done

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -1,16 +1,32 @@
 package edu.cmu.cs.cs214.rec04;
 
 /**
- * DelegationSortedIntList -- a variant of a SortedIntList that keeps
- * count of the number of attempted element insertions (not to be confused
- * with the current size, which goes down when an element is removed)
- * and exports an accessor (totalAdded) for this count.
+ * DelegationSortedIntList -- a variant of a SortedIntList that keeps count of
+ * the number of attempted element insertions (not to be confused with the
+ * current size, which goes down when an element is removed) and exports an
+ * accessor (totalAdded) for this count.
  *
  * @author Nora Shoemaker
  *
  */
+public class DelegationSortedIntList implements IntegerList {
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
+    @Override
+    public boolean add(int num);
+
+    @Override
+    public boolean addAll(IntegerList list);
+
+    @Override
+    public int get(int index);
+
+    @Override
+    public boolean remove(int num);
+
+    @Override
+    public boolean removeAll(IntegerList list);
+
+    @Override
+    public int size();
 
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -1,16 +1,45 @@
 package edu.cmu.cs.cs214.rec04;
 
 /**
- * InheritanceSortedIntList -- a variant of a SortedIntList that keeps
- * count of the number of attempted element insertions (not to be confused
- * with the current size, which goes down when an element is removed)
- * and exports an accessor (totalAdded) for this count.
+ * InheritanceSortedIntList -- a variant of a SortedIntList that keeps count of
+ * the number of attempted element insertions (not to be confused with the
+ * current size, which goes down when an element is removed) and exports an
+ * accessor (totalAdded) for this count.
  *
  * @author Nora Shoemaker
  *
  */
+public class InheritanceSortedIntList extends SortedIntList {
 
-public class InheritanceSortedIntList {
     // Write your implementation below with API documentation
+    private int totalAdded;
 
+    public InheritanceSortedIntList() {
+        super();
+        totalAdded = 0;
+
+    }
+
+    @Override
+    public boolean add(int num) {
+        boolean blabla = super.add(num);
+        if (blabla) {
+            totalAdded++;
+        }
+        return blabla;
+    }
+
+    @Override
+    public boolean addAll(IntegerList List) {
+        // int size = size();
+        boolean amjilt = super.addAll(List);
+        if (amjilt) {
+            totalAdded++;
+        }
+        return amjilt;
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
